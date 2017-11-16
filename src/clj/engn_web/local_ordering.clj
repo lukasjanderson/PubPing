aa789c60d20ee555e756ffb5b5da05b149fecefd
Luke Anderson <Luke Anderson>
2017-11-16 11:50:23 -0600

    menu format

--- src/clj/engn_web/local_ordering.clj
+++ src/clj/engn_web/local_ordering.clj
@@ -6,7 +6,7 @@
 ;; ==========================================================================
 
 (defn categories []
-  {"specialties" [{:id "Chicken Tenders" :class "entree" :price ""}
+  {"Specialties" [{:id "Chicken Tenders" :class "entree" :price ""}
                   {:id "Nashville Hot Chicken Sandwich" :class "entree" :price ""}
                   {:id "Southern Chicken Wrap" :class "entree" :price ""}
                   {:id "Jack & Cheddar Quesadilla" :price ""}
@@ -15,25 +15,25 @@
                   {:id "Veggie Quesadilla" :class "entree" :price ""}
                   {:id "Classic French Dip" :class "entree" :price ""}
                   {:id "Veggie Wrap" :class "entree" :price ""}
-                  {:id "Jumbo Wings" :class "entree" :price "" :sauce (sauces)}
+                  {:id "Jumbo Wings" :class "entree" :price ""} :sauce (sauces)
                   {:id "Spicy Pork Belly Sandwich" :class "entree" :price ""}
                   {:id "Pub Turkey Club" :class "entree" :price ""}]
-   "salads"      [{:id "Caesar Salad" :class "entree" :price ""}
+   "Salads"      [{:id "Caesar Salad" :class "entree" :price ""}
                   {:id "Caesar Salad w/ Tofu" :class "entree" :price ""}
                   {:id "Caesar Salad w/ Grilled Chicken" :class "entree" :price ""}
                   {:id "Cobb Salad" :class "entree" :price "" :dressing (dressings)}
                   {:id "Cobb Salad w/ Tofu" :class "entree" :price "" :dressing (dressings)}
                   {:id "Cobb Salad w/ Grilled Chicken" :class "entree" :price "" :dressing (dressings)}
                   {:id "Cobb Salad w/ Fried Chicken" :class "entree" :price "" :dressing (dressings)}]
-   "burgers"     [{:id "Pub Burger" :class "entree" :price "" :cheese (cheeses)}
+   "Burgers"     [{:id "Pub Burger" :class "entree" :price "" :cheese (cheeses)}
                   {:id "Pub Burger w/ Bacon" :class "entree" :price "" :cheese (cheeses)}
                   {:id "Vegan Burger" :class "entree" :price ""}]
-   "desserts"    [{:id "Chocolate Chip Cookie" :class "side" :price ""}
+   "Desserts"    [{:id "Chocolate Chip Cookie" :class "side" :price ""}
                   {:id "Ghirardelli Brownie" :class "side" :price ""}
                   {:id "Ghirardelli Brownie w/ Vanilla Ice Cream" :class "entree" :price ""}
                   {:id "Milkshake" :class "entree" :price "" :flavor (flavors)}
                   {:id "Root Beer Float" :class "entree" :price ""}]
-   "sides"       [{:id "Pub Fries" :class "side" :price ""}
+   "Sides"       [{:id "Pub Fries" :class "side" :price ""}
                   {:id "Onion Rings" :class "side" :price ""}
                   {:id "Tortilla Chips" :class "side" :price ""}
                   {:id "Kettle Chips" :class "side" :price ""}
@@ -43,7 +43,7 @@
                   {:id "Chips & Salsa" :class "side" :price ""}
                   {:id "Chips & Guacamole" :class "side" :price ""}
                   {:id "Chips & Queso" :class "side" :price ""}]
-   "drinks"      [{:id "Fountain Drink" :class "side" :price ""}
+   "Drinks"      [{:id "Fountain Drink" :class "side" :price ""}
                   {:id "Bottled Water" :class "side" :price ""}]})
 
 (defn sauces []
@@ -69,15 +69,6 @@
    :flavor "Strawberry"
    :flavor "Vanilla"
    :flavor "Special"})
-
-
-(defn messages-initial-state
-  []
-  {"default" [{:msg "CS 4278" :time 1 :user {:name "Your Name" :nickname "You"}}
-              {:msg "vandy" :time 1 :user {:name "Your Name" :nickname "You"}}]
-   "food" [{:msg "Eat at fido" :time 1 :user {:name "Me" :nickname "Me"}}]
-   "vandy" [{:msg "Visit the Wondr'y" :time 1 :user {:name "Your Name" :nickname "Bob"}}]})
-
 
 (defn messages-expand [msg expansions]
    (let [msg-text        (:msg msg)
